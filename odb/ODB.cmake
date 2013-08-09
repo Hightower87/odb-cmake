550b7e6b22177d95bab911238b18d1da3754e8a2
Per Edin <infO@peredin.com>
2013-08-09 04:29:58 +0200

    Changed license for ODB.cmake
    Changed the license terms for ODB.cmake from GPL to a less complex
    MIT-like license.

--- odb/ODB.cmake
+++ odb/ODB.cmake
@@ -9,20 +9,7 @@
 # You are encouraged to let me know if you find this file useful
 # in any way. :)
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
+# See COPYING for license terms.
 #
 # ----------------------------------------------------------------------
 
